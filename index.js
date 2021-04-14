80874545cf5a8258e1bb25f2cfb169ee44709520
Ralf Lopez <ralflopez3@gmail.com>
2021-04-15 00:15:47 +0800

    static config

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+;}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server running'));
