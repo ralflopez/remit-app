5281873561f71b27641969ffabd1f18c259f58a2
Ralf Lopez <ralflopez3@gmail.com>
2021-04-15 00:20:17 +0800

    import path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require('./routes');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
